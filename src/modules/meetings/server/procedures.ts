4e8a084b2b2db017e1937e5936a7e2c75eff652f
jeevan thapa <jeevanthapa@microsoft.com>
2025-11-06 13:12:27 -0500

    20: Meeting page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -9,6 +9,23 @@ import { meetingInsertSchema, meetingUpdateSchema } from "../schemas";
 import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
+  remove: protectedProcedure
+  .input(z.object({ id: z.string() }))
+  .mutation(async ({ input, ctx }) => {
+    const [deletedMeeting] = await db
+      .delete(meetings)
+      .where(
+        and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+      )
+      .returning();
+    if (!deletedMeeting) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: `Meeting with id ${input.id} not found`,
+      });
+    }
+    return deletedMeeting;
+  }),
   update: protectedProcedure
     .input(meetingUpdateSchema)
     .mutation(async ({ input, ctx }) => {
@@ -46,8 +63,11 @@ export const meetingsRouter = createTRPCRouter({
       const [existingMeeting] = await db
         .select({
           ...getTableColumns(meetings),
+          agents: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
         );
